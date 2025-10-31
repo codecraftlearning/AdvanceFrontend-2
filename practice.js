de0792b6e6a7d56e2885e499f1f0ed630d5e2540
bhavishya saini <Bhavishyasaini9@gmail.com>
2025-10-31 17:59:24 +0530

    commite

---
+++ practice.js
@@ -0,0 +1,19 @@
+let no = 6556;
+let divisor = 2
+let reminder = 0
+
+
+if (reminder === no % divisor){
+    console.log("even")
+}
+else {
+    console.log("odd")
+}
+
+
+
+//  1 print all even numbers  from zero to the range provided .
+//  2 print all odd numbers  from zero to the range provided .
+//  3 in a range from zero to provided range add all even numbers and subtract half of the sum of odd numbers .
+//**4 dont use internet for this **** check if a number provided is a prime no or not .
+   //"print prime if prime .composite if not prime"
