9718cc00ae570423b4a40ad256e663d8de5503e5
bhavishya saini <Bhavishyasaini9@gmail.com>
2025-10-31 15:44:12 +0530

    .

--- js classes/functions.js
+++ js classes/functions.js
@@ -27,3 +27,23 @@ console.log(sum);
 //homework -> adddition of cubes of 3 numbers
 //series sum -> 2, 10 times, print result
 
+// adddition of cubes of 3 numbers
+let a = 2;
+let b = 3;
+let c = 4;
+
+let sumOfCubes = a**3 + b**3 + c**3;
+
+console.log("adddition of cubes of 3 numbers", sumOfCubes);
+
+
+//series sum -> 2, 10 times, print result
+let num = 2;
+let times = 10;
+let sum = 0;
+
+for (let i = 1; i <= times; i++) {
+  sum += num;
+}
+console.log("The sum of the series is:", sum);
+
