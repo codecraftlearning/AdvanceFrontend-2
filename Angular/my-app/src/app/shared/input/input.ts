7fbcdfbb8345fcb53adc1c6b4718a172205b2f9d
codecraftlearning <codecraftlearning@gmail.com>
2026-01-22 19:01:34 +0530

    one way binding, two way binding

--- Angular/my-app/src/app/shared/input/input.ts
+++ Angular/my-app/src/app/shared/input/input.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './input.html',
   styleUrl: './input.scss',
 })
 export class Input {
-
+  public id: string = 'fullName_id';
+  public label: string = 'Full Name';
+  public type: string = 'password';
+  public name: string = 'fullName';
+  public placeholder: string = 'your name';
+  public value: string = 'current Value';
 }
